ad68054a348714fd94c4c2ea4ce1deccee51ff3b
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2023-11-25 19:50:54 +0530

    Use vite to crete app

--- CC/index.js
+++ CC/index.js
@@ -5,4 +5,10 @@ const FIXER_API_KEY = 'a7ea9efd9e4d05b6ce017faa90364655';
 const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
 
 // https://restcountries.com
-const REST_COUNTRIES_API = `https://restcountries.com/v3.1/currency/cop`;
+const REST_COUNTRIES_API = `https://restcountries.com/v3.1/currency`;
+
+
+const getExchangedRate = async (fromCurrency, toCurrency) => {
+    const {data:{rates}} = await axios.get(FIXER_API);
+
+}
