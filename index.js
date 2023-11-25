97cd38fd412c4927cbe8ac1a982c3ab6dd42da84
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2023-11-25 19:06:55 +0530

    Crating structure...

---
+++ index.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+
+// https://fixer.io/
+const FIXER_API_KEY = 'a7ea9efd9e4d05b6ce017faa90364655';
+const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
+
+// https://restcountries.com
+const REST_COUNTRIES_API = `https://restcountries.com/v3.1/currency/cop`;
